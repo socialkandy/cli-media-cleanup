658cfce9ec243d761a0f31287765577798e72d74
Breno Alves <breno.alvs@hotmail.com>
2017-11-20 08:24:55 -0200

    Removed unused arguments from test message

--- media-cleanup-command.php
+++ media-cleanup-command.php
@@ -20,7 +20,7 @@ class Media_Cleanup_Command {
 	 * @when after_wp_load
 	 */
 	public function __invoke( $args, $assoc_args ) {
-		WP_CLI::success( $args[0] . implode( ', ', $assoc_args ) );
+		WP_CLI::success( implode( ', ', $assoc_args ) );
 	}
 }
 WP_CLI::add_command( 'media cleanup', 'Media_Cleanup_Command' );
